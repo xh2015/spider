3f7021e2a17f638df6278477388b4c0ccda06c60
xuhao <980916277@qq.com>
2024-01-17 20:10:47 +0800

    类型融合

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
-import express, { Response, Request } from 'express';
+import express, { Response, Request, NextFunction } from 'express';
+import bodyParser from 'body-parser';
 import router from './router';
+import { next } from 'cheerio/lib/api/traversing';
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use((req: Request, res: Response, next: NextFunction) => {
+    req.teacherName = 'dell';
+    next();
+});
 app.use(router);
 
 app.listen(7001, () => {
--- src/router.ts
+++ src/router.ts
@@ -2,20 +2,40 @@ import { Router, Request, Response } from "express";
 import Crowller from './crowller';
 import DellAnalyzer from './dellAnalyzer'
 
+interface RequestWithBody extends Request {
+    body: {
+        [key: string]: string | undefined;//泛匹配
+    }
+}
+
 const router = Router();
 
-
 router.get('/', (req: Request, res: Response) => {
-    res.send('hello world');
+    res.send(`
+        <html>
+            <body>
+                <form mthod = "post" action="/getData">
+                    <input type="password" name="password"/>
+                    <button>提交</button>
+                </form>
+            </body>
+        </html>
+    `);
 });
 
-router.get('/getData', (req: Request, res: Response) => {
-    const secret = "secretKey";
-    const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`;
+router.get('/getData', (req: RequestWithBody, res: Response) => {
+    const { password } = req.body;
+    if (password === '123') {
+        const secret = "secretKey";
+        const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`;
 
-    const analyzer = DellAnalyzer.getInstance();
-    new Crowller(url, analyzer);
-    res.send('getData success!');
+        const analyzer = DellAnalyzer.getInstance();
+        new Crowller(url, analyzer);
+        res.send('getData Success!');
+    } else {
+        res.send(`${req.teacherName} password Error!`);
+    }
+
 });
 
 export default router;
