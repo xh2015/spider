75db8b3f37e80f8d13c1f8eece759d95270f51d7
gary.xu <xuhaozvv@163.com>
2024-01-18 21:55:57 +0800

    登录

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import express, { Response, Request, NextFunction } from 'express';
 import bodyParser from 'body-parser';
 import router from './router';
+import cookieSession from 'cookie-session';
 import { next } from 'cheerio/lib/api/traversing';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(cookieSession({
+    name: 'session',
+    keys: ['teacher dell'],
+    maxAge: 24 * 60 * 60 * 1000
+}))
 app.use(router);
 
 app.listen(7001, () => {
--- src/router.ts
+++ src/router.ts
@@ -11,21 +11,56 @@ interface RequestWithBody extends Request {
 const router = Router();
 
 router.get('/', (req: Request, res: Response) => {
-    res.send(`
+    const isLogin = req.session ? req.session.login : false;
+    if (isLogin) {
+        res.send(`
         <html>
             <body>
-                <form mthod = "post" action="/getData">
+                <a href='/getData' >爬取内容</a>
+                <a href='/logout' >退出</a>
+            </body>
+        </html>
+    `);
+    } else {
+        res.send(`
+        <html>
+            <body>
+                <form method = "post" action="/login">
                     <input type="password" name="password"/>
-                    <button>提交</button>
+                    <button>登录</button>
                 </form>
             </body>
         </html>
     `);
+    }
+});
+
+router.get('/logout', (req: Request, res: Response) => {
+    if (req.session) {
+        req.session.login = false;
+    }
+    res.redirect('/');
+});
+
+router.post('/login', (req: RequestWithBody, res: Response) => {
+    const { password } = req.body;
+    const isLogin = req.session ? req.session.login : false;
+    if (isLogin) {
+        res.send("已经登录过");
+    } else {
+        console.log(password, req.session);
+        if (password === '123' && req.session) {
+            req.session.login = true;
+            res.send("登录成功");
+        } else {
+            res.send("登录失败");
+        }
+    }
 });
 
 router.get('/getData', (req: RequestWithBody, res: Response) => {
-    const { password } = req.body;
-    if (password === '123') {
+    const isLogin = req.session ? req.session.login : false;
+    if (isLogin) {
         const secret = "secretKey";
         const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`;
 
@@ -33,7 +68,7 @@ router.get('/getData', (req: RequestWithBody, res: Response) => {
         new Crowller(url, analyzer);
         res.send('getData Success!');
     } else {
-        res.send('password Error!');
+        res.send('请先登录后再爬取!');
     }
 
 });
