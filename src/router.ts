4c3431ca097ae7b280674be860b510b6f81d5308
gary.xu <xuhaozvv@163.com>
2024-01-18 22:20:54 +0800

    展示爬取内容

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,6 @@
 import { Router, Request, Response } from "express";
+import fs from 'fs';
+import path from 'path';
 import Crowller from './crowller';
 import DellAnalyzer from './dellAnalyzer'
 
@@ -17,6 +19,7 @@ router.get('/', (req: Request, res: Response) => {
         <html>
             <body>
                 <a href='/getData' >爬取内容</a>
+                <a href='/showData' >展示内容</a>
                 <a href='/logout' >退出</a>
             </body>
         </html>
@@ -70,7 +73,21 @@ router.get('/getData', (req: RequestWithBody, res: Response) => {
     } else {
         res.send('请先登录后再爬取!');
     }
+});
 
+router.get('/showData', (req: RequestWithBody, res: Response) => {
+    const isLogin = req.session ? req.session.login : false;
+    if (isLogin) {
+        try {
+            const position = path.resolve(__dirname, '../data/course.json');
+            const result = fs.readFileSync(position, 'utf8');
+            res.json(JSON.parse(result));
+        } catch (e) {
+            res.send('尚未爬取到内容!');
+        }
+    } else {
+        res.send('登录后展示内容!');
+    }
 });
 
 export default router;
